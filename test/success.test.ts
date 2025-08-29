54e6a4d609f54d5a3a18d579dbf242df5100b330
Gabriel Alves <ga654066@gmail.com>
2025-08-29 12:16:23 -0300

    feat: vites config and tests

---
+++ test/success.test.ts
@@ -0,0 +1,9 @@
+import { describe, expect, it } from 'vitest';
+
+describe('My Test Suite success', () => {
+  it('Success', () => {
+    const ok: boolean = true;
+
+    expect(ok).toBe(true);
+  });
+});
