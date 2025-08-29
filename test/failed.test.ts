a7c64f6d2e365f0fd37c16a8a4c67b2b15b682eb
Gabriel Alves <ga654066@gmail.com>
2025-08-29 13:54:13 -0300

    chore: small adjustmmens

--- test/failed.test.ts
+++ test/failed.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, it } from 'vitest';
 
 describe('My Test Suite Faieled', () => {
   it('Failed', () => {
-    const ok: boolean = true;
+    const ok: boolean = false;
 
     expect(ok).toBe(true);
   });
